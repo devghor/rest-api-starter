8256b73f204cae0ed481e5703e37971eca4828c8
devghor <hafij.to@gmail.com>
2021-11-21 18:59:13 +0600

    Add dashboard resource

---
+++ app/Http/Controllers/Api/Dashboard/DashboardController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Controllers\Api\Dashboard;
+
+use App\Http\Controllers\Controller;
+use App\Models\Permission;
+use App\Models\Role;
+use App\Models\User;
+use App\Values\StatusValue;
+use Illuminate\Http\Request;
+
+class DashboardController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        try {
+            $userCount = User::count();
+            $roleCount = Role::count();
+            $permissionCount = Permission::count();
+            return response([
+                'data'=>[
+                    'totalUser' => $userCount,
+                    'totalRole' => $roleCount,
+                    'totalPermission' => $permissionCount,
+                ]
+            ], StatusValue::HTTP_OK);
+        } catch (\Exception $e) {
+            return response([
+                'error'=>[
+                    'message' => $e->getMessage(),
+                    'code' => StatusValue::HTTP_UNPROCESSABLE_ENTITY
+                ]
+            ], StatusValue::HTTP_UNPROCESSABLE_ENTITY);
+        }
+    }
+
+}
--- routes/api.php
+++ routes/api.php
@@ -7,10 +7,12 @@ use App\Http\Controllers\Api\User\UserController;
 use App\Http\Middleware\AdminMiddleware;
 use App\Http\Controllers\Api\Role\RoleController;
 use App\Http\Controllers\Api\Permission\PermissionController;
+use App\Http\Controllers\Api\Dashboard\DashboardController;
 
 Route::prefix('v1')->group(function () {
     Route::post('login', [LoginController::class, 'login']);
     Route::group(['middleware' => ['auth:api', AdminMiddleware::class]], function () {
+        Route::get('dashboard-details', [DashboardController::class, 'index']);
         Route::apiResource('users', UserController::class);
         Route::apiResource('roles', RoleController::class);
         Route::apiResource('permissions', PermissionController::class);
